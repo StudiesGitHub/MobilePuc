7f12ad45592ce5a2379a7cb20fe03c6425062ad7
StudiesGitHub <ncsaraf@outlook.com>
2024-12-01 20:17:52 -0300

    update

--- AmigoDoBairro/app/src/main/java/com/pucpr/amigodobairro/WelcomeActivity.java
+++ AmigoDoBairro/app/src/main/java/com/pucpr/amigodobairro/WelcomeActivity.java
@@ -1,13 +1,29 @@
 package com.pucpr.amigodobairro;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class WelcomeActivity extends AppCompatActivity {
+
+    ImageView imageView;
+    TextView textoAviso;
+    Button anteriorButton;
+    Button proximoButton;
+
+    int index = 0;
+    String []images = new String[]{"buraco","ruadanificada"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        imageView = findViewById(R.id.imageView);
+        textoAviso = findViewById(R.id.textoAviso);
+        anteriorButton = findViewById(R.id.anteriorButton);
+        proximoButton = findViewById(R.id.proximoButton);
     }
 }
